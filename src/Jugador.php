e1f139bc5ca54856524caf8fc46e87d737258ce8
SergioLlanas <llanas.121822@e.unavarra.es>
2021-03-11 10:19:14 +0100

    refactor del codigo

--- src/Jugador.php
+++ src/Jugador.php
@@ -6,18 +6,36 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class Jugador
 {
+    /**
+     * @var
+     */
     private $puntuacion;
     private $nombre;
 
+    /**
+     * @return mixed
+     */
     public function getPuntuacion(){
         return $this->puntuacion;
     }
+
+    /**
+     * @param int $nuevaPuntuacion
+     */
     public function setPuntuacion(int $nuevaPuntuacion){
         $this->puntuacion = $nuevaPuntuacion;
     }
+
+    /**
+     * @return mixed
+     */
     public function getNombre(){
         return $this->nombre;
     }
+
+    /**
+     * @param string $nombre
+     */
     public function setNombre(string $nombre){
         $this->nombre = $nombre;
     }
--- src/Tenis.php
+++ src/Tenis.php
@@ -6,19 +6,30 @@ namespace Deg540\PHPTestingBoilerplate;
 
 class Tenis
 {
+    /**
+     * @var Jugador
+     */
     public $jugador1;
     public $jugador2;
 
-    public function __construct($nombre1,$nombre2)
+    /**
+     * Tenis constructor.
+     * @param $nombreJugador1
+     * @param $nombreJugador2
+     */
+    public function __construct($nombreJugador1, $nombreJugador2)
     {
         $this->jugador1 = new Jugador();
         $this->jugador1->setPuntuacion(0);
-        $this->jugador1->setNombre($nombre1);
+        $this->jugador1->setNombre($nombreJugador1);
         $this->jugador2 = new Jugador();
         $this->jugador2->setPuntuacion(0);
-        $this->jugador2->setNombre($nombre2);
+        $this->jugador2->setNombre($nombreJugador2);
     }
 
+    /**
+     * @return string
+     */
     public function getScore()
     {
         if ($this->ha_ganado_1()) {
@@ -28,23 +39,26 @@ class Tenis
             return "Gana " . $this->jugador2->getNombre();
         }
         if($this->no_tienen_los_mismos_puntos()){
-            if($this->jugador1->getPuntuacion() > 45){
-                return $this->traducir_marcador($this->jugador1->getPuntuacion()) . " " . $this->jugador1->getNombre();
+            if($this->tiene_ventaja_jugador_1()){
+                return $this->traducir_puntuacion($this->jugador1->getPuntuacion()) . " " . $this->jugador1->getNombre();
             }
-            if($this->jugador2->getPuntuacion() > 45){
-                return $this->traducir_marcador($this->jugador2->getPuntuacion()) . " " . $this->jugador2->getNombre();
+            if($this->tiene_ventaja_jugador_2()){
+                return $this->traducir_puntuacion($this->jugador2->getPuntuacion()) . " " . $this->jugador2->getNombre();
             }
-            return $this->traducir_marcador($this->jugador1->getPuntuacion()) . " - " . $this->traducir_marcador($this->jugador2->getPuntuacion());
+            return $this->traducir_puntuacion($this->jugador1->getPuntuacion()) . " - " . $this->traducir_puntuacion($this->jugador2->getPuntuacion());
         }
         if($this->jugador1->getPuntuacion() == 45){
             return "Deuce";
         }
-        return $this->traducir_marcador($this->jugador1->getPuntuacion()) . " all";
+        return $this->traducir_puntuacion($this->jugador1->getPuntuacion()) . " all";
     }
 
+    /**
+     * @param string $nombreJugador
+     */
     public function wonPoint(string $nombreJugador)
     {
-        if($this->jugador1->getNombre() == $nombreJugador){
+        if($this->es_el_jugador_1($nombreJugador)){
             $puntuacion = $this->jugador1->getPuntuacion();
             if($puntuacion == 45 && $this->jugador2->getPuntuacion()>45){
                 $this->jugador2->setPuntuacion(45);
@@ -52,7 +66,7 @@ class Tenis
                 $this->jugador1->setPuntuacion($puntuacion+15);
             }
         }
-        if($this->jugador2->getNombre() == $nombreJugador){
+        if($this->es_el_jugador_2($nombreJugador)){
             $puntuacion = $this->jugador2->getPuntuacion();
             if($puntuacion == 45 && $this->jugador1->getPuntuacion()>45){
                 $this->jugador1->setPuntuacion(45);
@@ -63,30 +77,11 @@ class Tenis
 
     }
 
-    private function ha_ganado_1(){
-        if(($this->jugador1->getPuntuacion() > 45 && $this->jugador2->getPuntuacion() < 45) || ($this->jugador1->getPuntuacion() > 60)){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    private function ha_ganado_2(){
-        if(($this->jugador2->getPuntuacion() > 45 && $this->jugador1->getPuntuacion() < 45) || ($this->jugador2->getPuntuacion() > 60)){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    private function no_tienen_los_mismos_puntos(){
-        if($this->jugador1->getPuntuacion() != $this->jugador2->getPuntuacion()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    private function traducir_marcador($marcador){
+    /**
+     * @param $puntuacion
+     * @return string
+     */
+    private function traducir_puntuacion($puntuacion){
         $casosBase = array(
             0 => "Love",
             15 => "Fifteen",
@@ -95,14 +90,88 @@ class Tenis
             60 => "Advantage",
         );
         foreach ($casosBase as $clave => $valor) {
-            if($clave == $marcador){
+            if($clave == $puntuacion){
                 return $valor;
             }
         }
-        return $this->traducir_marcador($this->jugador1->getPuntuacion()) . " all";
     }
 
 
+    /* Funciones de comprobación en ifs para simplificar el codigo*/
+    /**
+     * @param $nombreJugador
+     * @return bool
+     */
+    private function es_el_jugador_1($nombreJugador){
+        if($this->jugador1->getNombre() == $nombreJugador){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param $nombreJugador
+     * @return bool
+     */
+    private function es_el_jugador_2($nombreJugador){
+        if($this->jugador2->getNombre() == $nombreJugador){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function tiene_ventaja_jugador_1(){
+        if($this->jugador1->getPuntuacion() > 45){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function tiene_ventaja_jugador_2(){
+        if($this->jugador2->getPuntuacion() > 45){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function ha_ganado_1(){
+        if(($this->jugador1->getPuntuacion() > 45 && $this->jugador2->getPuntuacion() < 45) || ($this->jugador1->getPuntuacion() > 60)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private function ha_ganado_2(){
+        if(($this->jugador2->getPuntuacion() > 45 && $this->jugador1->getPuntuacion() < 45) || ($this->jugador2->getPuntuacion() > 60)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    private function no_tienen_los_mismos_puntos(){
+        if($this->jugador1->getPuntuacion() != $this->jugador2->getPuntuacion()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
 }
